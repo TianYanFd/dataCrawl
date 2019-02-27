c3187e6e65d33d2b5b27542f3346ad9247fc3897
TianYanFd <47770470+TianYanFd@users.noreply.github.com>
2019-02-27 11:18:14 +0800

    Add files via upload

---
+++ src/com/nstars/constant/DataConstants.java
@@ -0,0 +1,25 @@
+package com.nstars.constant;
+
+public class DataConstants {
+	
+	/**
+	 * 西刺代理网地址
+	 * */
+	public static final String XICI_URL = "http://www.xicidaili.com/nn/";
+	
+	/**
+	 * 检验代理服务器可用性分析网址
+	 * */
+	public final static String VALID_URL = "http://www.chinamoney.com.cn/ags/ms/cm-u-notice-an/bIssuAn?typeCode=100041&isNewTab=0&limit=1&timeln=1&pageSize=30&sDate=2018-12-26&eDate=2018-12-26&pageNo=1";
+	
+	/**
+	 * 中国外汇交易中心，同业存单
+	 * */
+	public static final String NCD_URL = "http://www.chinamoney.com.cn/ags/ms/cm-u-notice-an/bIssuAn?typeCode=100041&isNewTab=0&limit=1&timeln=1";
+	
+	/**
+	 * 数据采集，最大尝试采集次数
+	 * */
+	public static final int REPEAT_CRAWLER_TIMES = 10;
+
+}
